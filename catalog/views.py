608e284cb177930d4469e06ba4356259ad84da55
lthanh <lthanh0711@gmail.com>
2022-08-17 16:34:33 +0700

    commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import generic
-from django.shortcuts import get_object_or_404
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
